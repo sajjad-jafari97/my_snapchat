35c809976622f9683010d454786d056591b6f51f
sajjd <sajjadjafari081@gmail.com>
2022-10-11 16:07:06 +0200

    Add: Travail de theo

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer, createNativeStackNavigator } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Text } from 'react-native';
+
+import { Screen1 } from './screens/Screen1.js'
+
+console.log(createStackNavigator)
+const Stack = createStackNavigator(); // Stack contains Screen & Navigator properties
 
 export default function App() {
   return (
-    
-    <View style={styles.container}>
-      <Header title="Welcome to React Native"/>
-       <Text style={heading}>Step One</Text>
-      <Text>Open up App.js to start working on your app !</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Screen1} />
+        <Stack.Screen name="Profile" component={Screen1} />
+      </Stack.Navigator>
+      <Text>Test text</Text>
+      <Screen1></Screen1>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
