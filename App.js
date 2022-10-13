54673c8b51d9b68b68e4c1ccc2b2fc9f6eab6b8a
Théo Guerre <guerre.theo@gmail.com>
2022-10-13 10:19:01 +0200

    chg - renamed screens

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ import { ScreenConnexion } from './screens/Connexion.js';
 import { ScreenUser } from './screens/User.js';
 import { ScreenHome } from './screens/Home.js';
 
-console.log(createStackNavigator);
 const Stack = createStackNavigator(); // Stack contains Screen & Navigator properties
 
 export default function App() {
@@ -18,12 +17,11 @@ export default function App() {
 
       <Stack.Navigator initialRouteName='Home' >
 
-      <Stack.Screen name="Inscription" component={ScreenInscription} />
-      <Stack.Screen name="Home" component={ScreenHome} />
-      <Stack.Screen name="Connexion" component={ScreenConnexion} />
-      <Stack.Screen name="User" component={ScreenUser} />
+        <Stack.Screen name="Inscription" component={ScreenInscription} />
+        <Stack.Screen name="Home" component={ScreenHome} />
+        <Stack.Screen name="Connexion" component={ScreenConnexion} />
+        <Stack.Screen name="User" component={ScreenUser} />
       </Stack.Navigator>
-      <Text>Test text</Text>
     </NavigationContainer>
   );
 }
